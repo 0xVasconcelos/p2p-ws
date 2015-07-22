22a9f8b26b40bf09267f55e5f6dfa1f0227ee04a
lucaslg26 <lucaslg26@yahoo.com.br>
2015-07-21 21:47:57 -0300

    Server & peers test

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-/* commit test */
+/* TODO: Aceitar e organizar clientes da mesma forma que no randompeer.js */
+
+/* TODO: Aceitar conexões em mais de uma porta */
+
+/* TODO: Unificar randompeer.js(atualmente trabalhando como cliente) e app.js(atualmente trabalhando como servidor) */
+
+
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+});
+
+ws.send('something');
+});
---
+++ randompeer.js
@@ -0,0 +1,23 @@
+/* TODO: Conectar a peers da lista de IP's adquirida do tracker */
+/* TODO: Achar uma mandeira de gerar um ID único para cada peer baseado no IP e anúncio */
+
+var WebSocket = require('ws');
+var peers = {};
+var peersid = ['011', '312', '12C', 'XYZ', 'HHX', 'ALO', 'MAR', 'CIA', 'NO.'];
+
+function peerConnect(i){
+    return function(){
+        peers[peersid[i]] = new WebSocket('ws://127.0.0.1:8080');
+        peers[peersid[i]].on('open', function(){
+            peers[peersid[i]].send(i);
+        });
+    }
+}
+
+for(var i in peersid){
+    peers[peersid[i]] = peerConnect(i);
+}
+
+for(var j in peersid){
+    peers[peersid[j]]();
+}
